32a4f4ecc527396d784ce7954aa2b905f3e4b720
Damian Wujec <wujecdamian12@gmail.com>
2026-05-16 21:20:55 +0200

    feat: add ErrorPage as errorElement route

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,11 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
+import ErrorPage from "./error/errorPage";
 
-const router = createBrowserRouter([{ path: "/", element: <Root /> }]);
+const router = createBrowserRouter([
+  { path: "/", element: <Root />, errorElement: <ErrorPage /> },
+]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
