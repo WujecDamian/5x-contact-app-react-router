01cb44079052497afe31bde7c5917e31a899152c
Damian Wujec <wujecdamian12@gmail.com>
2026-05-16 13:36:27 +0200

    refactor(main): supply Root as our root path element

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Root from "./routes/root";
 
-const router = createBrowserRouter([
-  { path: "/", element: <div>Hello world!</div> },
-]);
+const router = createBrowserRouter([{ path: "/", element: <Root /> }]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
