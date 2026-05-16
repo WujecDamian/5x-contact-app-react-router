4b79c42af64b3b94397d76166dfe3aa6e83bf623
Damian Wujec <wujecdamian12@gmail.com>
2026-05-16 22:56:39 +0200

    feat: integrate contact loader for dynamic data fetching in Contact component

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import Root, {
   action as rootAction,
 } from "./routes/root";
 import ErrorPage from "./error/errorPage";
-import Contact from "./routes/contact";
+import Contact, { loader as contactLoader } from "./routes/contact";
 
 const router = createBrowserRouter([
   {
@@ -16,7 +16,13 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     loader: rootLoader,
     action: rootAction,
-    children: [{ path: "contacts/:contactId", element: <Contact /> }],
+    children: [
+      {
+        path: "contacts/:contactId",
+        element: <Contact />,
+        loader: contactLoader,
+      },
+    ],
   },
 ]);
 
