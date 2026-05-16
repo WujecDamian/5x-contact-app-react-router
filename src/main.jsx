1ad799d89969a689df45d5ba2990d96bc8abdf04
Damian Wujec <wujecdamian12@gmail.com>
2026-05-16 21:29:48 +0200

    feat: add Contact component as a Root children

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,15 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
 import ErrorPage from "./error/errorPage";
+import Contact from "./routes/contact";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Root />, errorElement: <ErrorPage /> },
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [{ path: "contacts/:contactId", element: <Contact /> }],
+  },
 ]);
 
 createRoot(document.getElementById("root")).render(
