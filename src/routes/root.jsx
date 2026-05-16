841e73079df37783026b385af5442ed8975d709c
Damian Wujec <wujecdamian12@gmail.com>
2026-05-16 22:25:00 +0200

    feat: implement contact creation and update contact list rendering

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,5 +1,10 @@
-import { Link, Outlet } from "react-router-dom";
-import { getContacts } from "../contacts";
+import { Link, Outlet, useLoaderData, Form } from "react-router-dom";
+import { getContacts, createContact } from "../contacts";
+
+export async function action() {
+  const contact = await createContact();
+  return { contact };
+}
 
 export async function loader() {
   const contacts = await getContacts();
@@ -7,6 +12,7 @@ export async function loader() {
 }
 
 export default function Root() {
+  const { contacts } = useLoaderData();
   return (
     <>
       <div id="sidebar">
@@ -28,14 +34,28 @@ export default function Root() {
           </form>
         </div>
         <nav>
-          <ul>
-            <li>
-              <Link to={"/contacts/1"}>Your Name</Link>
-            </li>
-            <li>
-              <Link to={"/contacts/2"}>Your Friend</Link>
-            </li>
-          </ul>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No name</i>
+                    )}{" "}
+                    {contact.favorite && <span>*</span>}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
         </nav>
       </div>
       <div id="outlet">
