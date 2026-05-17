ba308130967e9ddd816a1e1c402ee1aaaa9cdb4b
Damian Wujec <wujecdamian12@gmail.com>
2026-05-17 14:03:40 +0200

    feat: add Index site to show on page open instead of blank white site

---
+++ src/routes/index.jsx
@@ -0,0 +1,16 @@
+export default function Index() {
+  return (
+    <p id="zero-state">
+      This is a demo for React Router.
+      <br />
+      Check out{" "}
+      <a
+        href="https://github.com/WujecDamian/5x-contact-app-react-router#"
+        target="_blank"
+      >
+        and Feel free to come to my GitHub
+      </a>
+      .
+    </p>
+  );
+}
